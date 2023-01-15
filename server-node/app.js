be294fc0fcec88ada9b9a99251bb37910f8254e6
tekeuange23 <tekeuangeleonel@gmail.com>
2023-01-15 15:57:45 +0100

    starting mysql connection with fastify with @fastify/mysql

--- server-node/app.js
+++ server-node/app.js
@@ -1,5 +1,6 @@
 import Fastify from 'fastify'
 import * as dotenv from 'dotenv'
+import fastifyMysql from '@fastify/mysql'
 
 // Routes
 import UserRoute from './routes/user.routes.js'
@@ -11,19 +12,32 @@ dotenv.config()
 
 for (const [i, port] of [3000, 3001, 3002].entries()) {
   fastify[i] = Fastify({
-    logger: true
+    logger: false
   })
-  
-  fastify[i].get('/', async (request, reply) => {
-    reply.type('application/json').code(200)
-    return { hello: 'world' }
+
+  // MySQL connection
+  // URI format  ---->    mysql://user:password@hostname:port/database
+  const { DB_HOST, DB_NAME, DB_PORT, DB_USER, DB_PASS, DB_DIALECT } = process.env
+  const connectionString = DB_DIALECT + '://' + DB_USER + ':' + DB_PASS + '@' + DB_HOST + ':' + DB_PORT + '/' + DB_NAME
+  console.log(connectionString)
+
+  fastify[i].register(fastifyMysql, {
+    connectionString
   })
 
   // Route registering
+  fastify[i].get('/', async (request, reply) => {
+    reply.type('application/json').code(200)
+    console.log(Object.keys(fastify[i]).sort());
+    const connection = fastify[i].mysql.getConnection()
+    
+    return Object.keys(fastify[i].mysql)
+  })
+
   fastify[i].register(UserRoute, { logLevel: 'info' })
   fastify[i].register(TransactionRoute, { logLevel: 'info' })
   
-  // 
+  // Listening
   fastify[i].listen({ port }, (err, address) => {
     if (err) throw err
   })
--- server-node/routes/user.routes.js
+++ server-node/routes/user.routes.js
@@ -1,7 +1,14 @@
 import { index } from '../controllers/user.js'
 
 export default function (fastify, opts, done) {
-  fastify.get('/users', index)
+  // fastify.get('/users', index)
+
+  fastify.get('/users', async function index(request, reply) {
+    // const connection = await fastify?.mysql?.getConnection()
+    // const [rows, fields] = await connection.query('SELECT * FROM user')
+    // connection.release()
+    // return rows[0]
+  })
   
   done()
 }
